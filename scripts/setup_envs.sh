362353ad84ab6d2bfdca677d61b1ed98f858fa3e
JinhuaLiang <liangjh0903@gmail.com>
2025-02-15 07:16:44 +0000

    update setup

--- scripts/setup_envs.sh
+++ scripts/setup_envs.sh
@@ -2,3 +2,17 @@ conda env create -f venvs/audiocraft.yml
 conda env create -f venvs/audioldm.yml
 conda env create -f venvs/audiosr.yml
 conda env create -f venvs/wavcraft.yml
+# Prepare third-party repos
+# Comment some of them if they are unnecessary
+cd ext/
+
+git clone https://github.com/haoheliu/AudioLDM.git
+
+git clone https://github.com/Audio-AGI/AudioSep.git
+
+wget https://uplex.de/audiowmark/releases/audiowmark-0.6.1.tar.gz
+tar -xzvf audiowmark-0.6.1.tar.gz
+cd audiowmark-0.6.1
+./configure
+make
+make install
